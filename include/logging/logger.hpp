18b007ed41ec32111bd0eacf1c69bbe507aaea4d
tungpd <tungphamdoan2509@gmail.com>
2026-01-12 22:12:46 -0500

    Update logger

--- include/logging/logger.hpp
+++ include/logging/logger.hpp
@@ -5,6 +5,8 @@
 #include <spdlog/sinks/stdout_color_sinks.h>
 #include <spdlog/spdlog.h>
 #include <string>
+#include <string_view>
+#include <utility>
 
 namespace tnn {
 
@@ -18,39 +20,42 @@ public:
   ~Logger() = default;
 
   void set_level(LogLevel level);
-
   void set_log_file(const std::string &log_file);
-
   void enable_console_logging(bool enable = true);
 
-  template <typename... Args> void trace(const char *fmt, Args &&...args) {
+  template <typename... Args> void trace(spdlog::format_string_t<Args...> fmt, Args &&...args) {
     if (logger_)
-      logger_->trace(fmt::runtime(fmt), std::forward<Args>(args)...);
+      logger_->trace(fmt, std::forward<Args>(args)...);
   }
 
-  template <typename... Args> void debug(const char *fmt, Args &&...args) {
+  template <typename... Args> void debug(spdlog::format_string_t<Args...> fmt, Args &&...args) {
     if (logger_)
-      logger_->debug(fmt::runtime(fmt), std::forward<Args>(args)...);
+      logger_->debug(fmt, std::forward<Args>(args)...);
   }
 
-  template <typename... Args> void info(const char *fmt, Args &&...args) {
+  template <typename... Args> void info(spdlog::format_string_t<Args...> fmt, Args &&...args) {
     if (logger_)
-      logger_->info(fmt::runtime(fmt), std::forward<Args>(args)...);
+      logger_->info(fmt, std::forward<Args>(args)...);
   }
 
-  template <typename... Args> void warn(const char *fmt, Args &&...args) {
+  template <typename... Args> void warn(spdlog::format_string_t<Args...> fmt, Args &&...args) {
     if (logger_)
-      logger_->warn(fmt::runtime(fmt), std::forward<Args>(args)...);
+      logger_->warn(fmt, std::forward<Args>(args)...);
   }
 
-  template <typename... Args> void error(const char *fmt, Args &&...args) {
+  template <typename... Args> void error(spdlog::format_string_t<Args...> fmt, Args &&...args) {
     if (logger_)
-      logger_->error(fmt::runtime(fmt), std::forward<Args>(args)...);
+      logger_->error(fmt, std::forward<Args>(args)...);
   }
 
-  template <typename... Args> void critical(const char *fmt, Args &&...args) {
+  template <typename... Args> void critical(spdlog::format_string_t<Args...> fmt, Args &&...args) {
     if (logger_)
-      logger_->critical(fmt::runtime(fmt), std::forward<Args>(args)...);
+      logger_->critical(fmt, std::forward<Args>(args)...);
+  }
+
+  void log_runtime(LogLevel level, std::string_view msg) {
+    if (logger_)
+      logger_->log(level, msg);
   }
 
 private:
@@ -67,35 +72,43 @@ private:
 
 public:
   static void set_level(LogLevel level) { instance().set_level(level); }
-
   static void set_log_file(const std::string &log_file) { instance().set_log_file(log_file); }
-
   static void enable_console_logging(bool enable = true) {
     instance().enable_console_logging(enable);
   }
 
-  template <typename... Args> static void trace(const char *fmt, Args &&...args) {
+  template <typename... Args>
+  static void trace(spdlog::format_string_t<Args...> fmt, Args &&...args) {
     instance().trace(fmt, std::forward<Args>(args)...);
   }
 
-  template <typename... Args> static void debug(const char *fmt, Args &&...args) {
+  template <typename... Args>
+  static void debug(spdlog::format_string_t<Args...> fmt, Args &&...args) {
     instance().debug(fmt, std::forward<Args>(args)...);
   }
 
-  template <typename... Args> static void info(const char *fmt, Args &&...args) {
+  template <typename... Args>
+  static void info(spdlog::format_string_t<Args...> fmt, Args &&...args) {
     instance().info(fmt, std::forward<Args>(args)...);
   }
 
-  template <typename... Args> static void warn(const char *fmt, Args &&...args) {
+  template <typename... Args>
+  static void warn(spdlog::format_string_t<Args...> fmt, Args &&...args) {
     instance().warn(fmt, std::forward<Args>(args)...);
   }
 
-  template <typename... Args> static void error(const char *fmt, Args &&...args) {
+  template <typename... Args>
+  static void error(spdlog::format_string_t<Args...> fmt, Args &&...args) {
     instance().error(fmt, std::forward<Args>(args)...);
   }
 
-  template <typename... Args> static void critical(const char *fmt, Args &&...args) {
+  template <typename... Args>
+  static void critical(spdlog::format_string_t<Args...> fmt, Args &&...args) {
     instance().critical(fmt, std::forward<Args>(args)...);
+  }
+
+  static void log_runtime(LogLevel level, std::string_view msg) {
+    instance().log_runtime(level, msg);
   }
 };
 
