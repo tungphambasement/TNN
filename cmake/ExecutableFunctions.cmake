ae01e422e76879b9bcc066c79810d35e622b896a
tungpd <tungphamdoan2509@gmail.com>
2026-01-25 19:22:13 -0500

    Condense build system

--- cmake/ExecutableFunctions.cmake
+++ cmake/ExecutableFunctions.cmake
@@ -1,29 +1,27 @@
 # Create an executable with proper linking and configurations
-function(create_default target_name source_file)
+function(create_executable source_file)
+    # Derive target name from source file (remove extension)
+    get_filename_component(target_name ${source_file} NAME_WE)
+    
+    cmake_parse_arguments(
+        ARG
+        "USE_CUDA"                   
+        "LINK_MODE"
+        "LIBS"
+        ${ARGN}
+    )
+    
     add_executable(${target_name} ${source_file})
     
-    # Link core TNN libraries
-    target_link_libraries(${target_name} PRIVATE 
-        tnn_lib
-    )
-endfunction()
-
-function(create_base_executable target_name source_file)
-    add_executable(${target_name} ${source_file})
+    if(NOT ARG_LINK_MODE)
+        set(ARG_LINK_MODE PRIVATE)
+    endif()
     
-    target_link_libraries(${target_name} PRIVATE 
-        tnn_nn
-    )
-endfunction()
-
-function(create_cuda target_name source_file)
-    if(ENABLE_CUDA)
-        add_executable(${target_name} ${source_file})
-        
-        # Link core TNN libraries
-        target_link_libraries(${target_name} PRIVATE 
-            tnn_lib
-            CUDA::cudart
-        )
+    if(ARG_LIBS)
+        target_link_libraries(${target_name} ${ARG_LINK_MODE} ${ARG_LIBS})
+    endif()
+    
+    if(ARG_USE_CUDA AND ENABLE_CUDA)
+        target_link_libraries(${target_name} PRIVATE CUDA::cudart)
     endif()
 endfunction()
---
+++ examples/tensor_usage_test.cpp
@@ -0,0 +1,34 @@
+#include "tensor/tensor.hpp"
+
+using namespace tnn;
+
+using namespace std;
+
+void test_tensor_assignment(Tensor &a, Tensor &b) {
+  Tensor temp;
+  temp = a;
+  a = b;
+  b = temp;
+}
+
+void test_tensor_swap(Tensor &a, Tensor &b) { std::swap(a, b); }
+
+signed main() {
+  Tensor a = make_tensor<float>({64, 32, 32, 3});
+  Tensor b = make_tensor<float>({128, 224, 224, 3});
+
+  std::cout << "Tensor a shape: " << a->shape_str() << std::endl;
+  std::cout << "Tensor b shape: " << b->shape_str() << std::endl;
+
+  test_tensor_assignment(a, b);
+
+  std::cout << "After assignment:" << std::endl;
+  std::cout << "Tensor a shape: " << a->shape_str() << std::endl;
+  std::cout << "Tensor b shape: " << b->shape_str() << std::endl;
+
+  test_tensor_swap(a, b);
+  std::cout << "After swap:" << std::endl;
+  std::cout << "Tensor a shape: " << a->shape_str() << std::endl;
+  std::cout << "Tensor b shape: " << b->shape_str() << std::endl;
+  return 0;
+}
