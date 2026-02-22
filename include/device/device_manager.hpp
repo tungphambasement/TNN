1057d35568fe30f60d97d3b1b5f7f266d0c39a86
tungpd <tungphamdoan2509@gmail.com>
2026-02-21 23:24:59 -0500

    Add licensing

--- include/device/device_manager.hpp
+++ include/device/device_manager.hpp
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2025 Tung D. Pham
+ *
+ * This software is licensed under the MIT License. See the LICENSE file in the
+ * project root for the full license text.
+ */
 #pragma once
 
 #include <string>
